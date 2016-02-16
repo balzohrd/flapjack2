b4d54d5c10a465c7fa7e59f082ebf0d67a792996
Austin Turnage <austin@datarats.com>
2016-02-15 18:49:18 -0800

    cleanup

--- app.js
+++ app.js
@@ -6,43 +6,48 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
-var http = require('http');
 var path = require('path');
-
 var app = express();
+
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'jade');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+
+// OLD
+// app.use(express.favicon());
+// app.use(express.logger('dev'));
+// app.use(express.bodyParser());
+// app.use(express.methodOverride());
+// app.use(app.router);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/public', express.static(__dirname + "/public"));
 
+// OLD
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+// if ('development' == app.get('env')) {
+//   // app.use(express.errorHandler());
+// }
 
 app.get('/', routes.index);
+
 app.get('/users', user.list);
 
 app.get('/archive', function (req, res) {
-  res.render('archive.jade');
+  res.render('archive');
 });
 
 app.get('/about', function (req, res) {
-  res.render('about.jade');
+  res.render('about');
 });
 
 app.get('/contributors', function (req, res) {
-  res.render('contributors.jade');
+  res.render('contributors');
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
-});
+})
